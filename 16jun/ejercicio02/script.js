6cdc853b6c32d938ee3184af1e26c4dfeda61c3b
RubénLR95 <ruben.lopez.ras@gmail.com>
2025-06-16 16:37:27 +0200

    ejercicios 01,02,03

---
+++ 16jun/ejercicio02/script.js
@@ -0,0 +1,11 @@
+
+//"let" ASIGNA VALOR A LA VARIABLE//
+let numero = parseInt(prompt("Por favor, introduce un numero entero:"));
+// CONDICIONAL: SI AL DIVIDIRLO POR EL RESIDUO ES 0//
+if(numero % 2 ===0){
+    console.log(`${numero} es par`);
+    //EN CASO DE TENER RESIDUO SEÍA IMPAR//
+
+}else{
+    console.log(`${numero} es impar`);
+}
---
+++ 16jun/ejercicio03/script.js
@@ -0,0 +1,17 @@
+// Lógica booleana
+// True=verdadero, False=falso
+
+
+
+let llueveHoy = true;
+let tengoparaguas = false;
+
+if(llueveHoy){
+    if(tengoparaguas){
+        console.log("Salgo con paraguas");
+    } else{
+        console.log("No tengo paraguas, busco un refugio o compro uno");
+    }
+} else{
+    console.log("no llueve, salgo sin paraguas");
+}
