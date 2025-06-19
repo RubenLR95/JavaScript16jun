82b57794e7da5bd7f0693be228143cda1227af1a
RubénLR95 <ruben.lopez.ras@gmail.com>
2025-06-19 10:43:02 +0200

    ejercicios en console

---
+++ 19jun/Console-ejercicios/script.js
@@ -0,0 +1,52 @@
+// 1. SALUDO PERSONALIZADO
+//DEFINE LA FUNCIÓN SALUDAR
+
+// function saludar(nombre){
+//     return `¡Hola, ${nombre}!`;
+// }
+
+// // PRUEBA EN CONSOLA:
+
+// console.log(saludar('Ruben'));
+
+//2. SUMA DE DOS NUMEROS
+
+
+// function sumar(p1, p2){
+//    return p1 + p2;
+
+// }
+
+// console.log(sumar(3, 7))
+
+//3. VERIFICAR PAR O IMPAR
+
+// function esPar(num){
+//     return num % 2 === 0;
+
+// }
+
+// console.log(esPar( num ));
+
+//4.  LONGITUD DE UNA CADENA
+// FUNCION PARA CONTAR CARACTERES DE UN TEXTO
+
+// function longitud(cadena){
+//     return cadena.length;
+// }
+// console.log(longitud('Ruben'));
+
+//5. REPETIR UN TEXTO 
+
+// function repetir(text, num){
+//     let resultado = '';
+//     for (let i = 0; i < num; i++){
+//         resultado += text; 
+//     }
+//     return resultado;
+    
+// }
+
+// console.log(repetir('ha', 5));
+
+
