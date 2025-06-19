a825c7ef002f38c5cc30b7c66523653c7a754a3d
RubénLR95 <ruben.lopez.ras@gmail.com>
2025-06-19 13:06:03 +0200

    ejercicios propuestos

---
+++ 19jun/ejercicios propuestos/script.js
@@ -0,0 +1,63 @@
+//1. FACTORIAL DE UN NÚMERO
+
+// function factorial(n){
+//     return Math.(n);
+// }
+
+//  console.log(factorial(10).toFixed(2));
+
+//2. CONTAR VOCALES
+// function contarVocales(cadena){
+//     const vocales = "aeiou AEIOU";
+//     let  contador = 0;
+
+//     for (let char of cadena){
+//         if(vocales.includes(char)) contador++;
+            
+//     }
+//     return contador;
+
+// }
+// console.log(contarVocales("Ruben"));
+
+//3. CONVERTIR GRADOS CELSIUS A FAHRENHEIT
+// function celsiusF(cel) {
+//     resultado = (cel * 9/5) + 32 ;
+//     return resultado;
+// }
+
+// console.log(celsiusF(7).toFixed(2));
+
+//4. SUMAR
+
+// function sumar(num1, num2){
+//     resultado = num1+num2;
+//     return resultado;
+// }
+// console.log(sumar(7, 7));
+
+//5.MULTIPLICAR 
+
+// function multiplicar(num1, num2){
+//     resultado = num1 * num2;
+//     return resultado;
+// }
+// console.log(multiplicar(8, 5));
+
+
+//6. RESTAR
+// function restar(num1, num2){
+//     resultado = num1 - num2;
+//     return resultado;
+// }
+// console.log(restar(8, 5));
+
+//7. BOOLEANO INVERSO
+
+// function invertir(b){
+
+//     return !b;
+//  }
+// console.log(invertir(true, false));
+
+ 
